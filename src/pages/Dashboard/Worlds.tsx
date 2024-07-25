094f65dc4994461942856ae9d9a2cbf05b32443b
AlirieGray <alirie.gray@gmail.com>
2024-07-25 00:33:15 -0700

    add no worlds prompt

--- src/pages/Dashboard/Worlds.tsx
+++ src/pages/Dashboard/Worlds.tsx
@@ -1,12 +1,20 @@
 import WorldCard from './WorldCard'
 import './worlds.css'
 import { World } from '../../types'
+import { Link } from 'react-router-dom'
 
 // TODO: type safety props
 function Worlds({worlds, setModalOpen, setWorldToDelete, setWorldIDToDelete}: any) {
 
     return (
         <div className='worldsContainer'>
+            {worlds.length === 0 && (
+                <div className="noWorlds">
+                    {"You don't have any World States yet! "} 
+                    <Link className="createLink"to="/create">Click here</Link>
+                    {" to create one."}
+                </div>
+            )}
             {worlds && worlds.map((world: World) => {
                 return <WorldCard key={world.ID} 
                     world={world}
